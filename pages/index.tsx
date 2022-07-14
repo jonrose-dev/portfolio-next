99d83055f17521832f72232c77078b22f77e01ed
Jon <jon@jonrose.dev>
2022-07-13 20:50:21 -0400

    Fix layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ const Home: NextPage = () => {
       </Head>
       <div className="flex flex-col items-center min-h-screen bg-[linear-gradient(135deg,#007ACC_0%,#00D4FF_100%)] justify-between">
         <Text as="h1" size="xl" className="text-white text-center py-4">
-          Welcome
+          Oh, Hello There!
         </Text>
         <TerminalEmulator />
         <div className="h-20 relative flex justify-center">
@@ -41,10 +41,10 @@ const Home: NextPage = () => {
       <div className="h-fit bg-black grid grid-rows-[auto,1fr] w-full pb-12" ref={meSectionRef}>
         <Image
           src="/images/me.webp"
-          height={378}
-          width={378}
+          height={398}
+          width={398}
           alt="cartoon version of me at a computer"
-          className="w-[50vw] max-w-screen-sm justify-self-center text-center"
+          className="justify-self-center text-center"
         />
         <Text as="h2" size="lg" className="text-white w-[70vw] text-center justify-self-center">
           My name is Jon Rose and I am passionate about writing performant and elegant code. Whether
@@ -78,15 +78,24 @@ const Home: NextPage = () => {
         </div>
       </div>
       <div
-        className="h-fit min-h-screen bg-[#156620] flex flex-col items-center justify-between w-full pb-12  py-8"
+        className="h-fit min-h-screen bg-[#156620] flex flex-col gap-8 items-center w-full pb-12  py-8"
         ref={connectSectionRef}
       >
         <Text as="h2" size="xl" className="text-white w-[70vw] text-center justify-self-center">
           Let&apos;s Chat!
         </Text>
-        <Text className="text-white text-center" size="md">
-          I&apos;m always looking for new and exciting challenges
-        </Text>
+        <a
+          className="text-center items-center text-white flex flex-col gap-y-4"
+          href="https://jonrose.dev/resume.pdf"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="Resume"
+        >
+          <Text className="text-white text-center gap-4 flex items-center" size="md">
+            Check out my resume
+            <FileText className="text-center" size="24" />
+          </Text>
+        </a>
         <div className="flex gap-x-4 justify-center">
           <a
             href="https://www.linkedin.com/in/jonathan-rose-dev/"
@@ -126,18 +135,6 @@ const Home: NextPage = () => {
             />
           </a>
         </div>
-        <a
-          className="text-center items-center text-white flex flex-col gap-y-4"
-          href="https://jonrose.dev/resume.pdf"
-          target="_blank"
-          rel="noreferrer"
-          aria-label="Resume"
-        >
-          <Text className="text-white text-center" size="md">
-            Check out my resume
-          </Text>
-          <FileText className="text-center" size="48" />
-        </a>
         <Image
           src="/images/bye.webp"
           alt="Me Waving Goodbye"
